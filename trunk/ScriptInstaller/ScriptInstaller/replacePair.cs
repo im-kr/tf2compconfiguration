b8d922b87f1530c0c2adab2ba7bbab192c43c2ce
keithdanielharvey <keithdanielharvey@3abe2cdc-f47f-11dd-88c7-29a3b14d5316>
2009-02-16 06:59:14 +0000

    Latest Source code

---
+++ trunk/ScriptInstaller/ScriptInstaller/replacePair.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScriptInstaller
+{
+    struct ReplacePair
+    {
+        private string _replaceMe;
+
+        public string ReplaceMe
+        {
+            get { return _replaceMe; }
+            set { _replaceMe = value; }
+        }
+        private string _newLine;
+
+        public string NewLine
+        {
+            get { return _newLine; }
+            set { _newLine = value; }
+        }
+    }
+}
